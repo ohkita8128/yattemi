c883d765af132cf45021e6615b1eb89290e37c00
ohkita8128 <ohkita.katsuki.g7@s.mail.nagoya-u.ac.jp>
2026-01-29 12:24:54 +0900

    metaタグ追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,7 @@ export default function RootLayout({
     <html lang="ja" suppressHydrationWarning>
       <head>
         <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="YatteMi!" />
